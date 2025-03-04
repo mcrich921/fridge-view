adb981473e516628b3a0134f8cc302bbed138689
mrichw <mrich@wesleyan.edu>
2025-03-04 11:40:41 -0500

    added link from homepage to settings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Settings from "./pages/Settings";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/settings" element={<Settings />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
+
+function Home() {
+  return <Link to="/settings">Settings</Link>;
+}
+
+export default Home;
